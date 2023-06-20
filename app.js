0cda72752e1735173b27b167f1ab94513727fa71
mohammadbq <iiprxh@gmail.com>
2023-06-20 13:14:39 +0300

    re pushed the task and fixed an error

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const PORT = 8000   
-const express = require("express")
-const app = express()
-const products = require("./data")
+const PORT = 8000;
+const express = require("express");
+const data = require("./data");
+const app = express();
+
+app.use(express.json());
 
 app.get("/api/products", (req, res) => {
-    res.status (404) .json ({ data: data }) })
-    
+  console.log(`localhost: ${PORT}/api/products`);
+  return res.status(200).json({ data: data });
+});
 
-
-app.listen(PORT, () =>{
-    console.log(`My server is running ! :${PORT}`)
-
-})
+app.listen(PORT, () => {
+  console.log(`server is running at port: ${PORT}\nlocalhost:${PORT}`);
+});
